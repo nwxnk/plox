b63e9d6604f1919bbfa7a61f61984e2487c7519f
nwxnk <nwxnk@yandex.com>
2020-05-07 22:40:47 +0300

    fuck tab indentation

--- plox/stmts.py
+++ plox/stmts.py
@@ -3,26 +3,26 @@
 __all__ = ('Statement', 'VarStatement', 'PrintStatement', 'ExpressionStatement')
 
 class Statement:
-	pass
+    pass
 
 class PrintStatement(Statement):
-	def __init__(self, expr):
-		self.expression = expr
+    def __init__(self, expr):
+        self.expression = expr
 
-	def accept(self, visitor):
-		visitor.visit_print_statement(self)
+    def accept(self, visitor):
+        visitor.visit_print_statement(self)
 
 class ExpressionStatement(Statement):
-	def __init__(self, expr):
-		self.expression = expr
+    def __init__(self, expr):
+        self.expression = expr
 
-	def accept(self, visitor):
-		visitor.visit_expression_statement(self)
+    def accept(self, visitor):
+        visitor.visit_expression_statement(self)
 
 class VarStatement(Statement):
-	def __init__(self, name, expr):
-		self.name = name
-		self.initializer = expr
+    def __init__(self, name, expr):
+        self.name = name
+        self.initializer = expr
 
-	def accept(self, visitor):
-		visitor.visit_var_statement(self)
+    def accept(self, visitor):
+        visitor.visit_var_statement(self)
